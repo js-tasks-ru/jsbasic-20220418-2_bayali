73c32a9a896e5d158bb688e056eb9256113731ae
bayali12 <zloo095@yandex.ru>
2022-05-01 17:45:03 +0300

    Выполнил задания 4 модуля

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let cell = 0;
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].cells[cell].style.backgroundColor = 'red';
+    cell++;
+  }
 }
